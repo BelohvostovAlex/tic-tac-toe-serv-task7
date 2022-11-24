8a8560f4edeffa6e343cedab230e04c6a03f3052
Alex <belohvostov.belohvostov@gmail.com>
2022-11-24 14:43:15 +0300

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 import authRouter from "./routers/authRouter.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -18,7 +17,9 @@ app.use("/api", authRouter);
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URL);
-    app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server started on`)
+    );
   } catch (error) {
     console.log(error);
   }
